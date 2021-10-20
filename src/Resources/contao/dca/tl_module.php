0c9e04308c55361bf267a4f56b63e8892974769d
Alex Wuttke <alex@das-l.de>
2021-10-20 23:14:33 +0200

    Allow custom module templates

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -11,11 +11,11 @@ declare(strict_types=1);
  */
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['ivm_pro_list'] =
-    '{title_legend},name,type;{redirect_legend},jumpTo'
+    '{title_legend},name,type;{redirect_legend},jumpTo;{template_legend:hide},customTpl'
 ;
 $GLOBALS['TL_DCA']['tl_module']['palettes']['ivm_pro_reader'] =
-    '{title_legend},name,type'
+    '{title_legend},name,type;{template_legend:hide},customTpl'
 ;
 $GLOBALS['TL_DCA']['tl_module']['palettes']['ivm_pro_filter'] =
-    '{title_legend},name,type'
+    '{title_legend},name,type;{template_legend:hide},customTpl'
 ;
